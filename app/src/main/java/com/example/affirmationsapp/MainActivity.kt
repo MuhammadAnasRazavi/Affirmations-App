2b57dabd1456d67d1c42fb30a975463d5118e83b
MuhammadAnasRazavi <muhammadanasrazavi@gmail.com>
2023-11-30 13:52:38 +0530

    Added Affirmations List in vertical layout.

--- app/src/main/java/com/example/affirmationsapp/MainActivity.kt
+++ app/src/main/java/com/example/affirmationsapp/MainActivity.kt
@@ -1,15 +1,35 @@
 package com.example.affirmationsapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmationsapp.data.DataSource
+import com.example.affirmationsapp.model.Affirmation
 import com.example.affirmationsapp.ui.theme.AffirmationsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +42,98 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AffirmationsApp()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun AffirmationsAppLayout(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(stringResource(id = R.string.app_name))
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Developed by @Muhammad Anas Razavi",
+                )
+            }
+        }
+    ) { innerPadding ->
+        AffirmationsApp(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = affirmation.imageResourceId),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(194.dp),
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            text = stringResource(id = affirmation.stringResourceId),
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.headlineSmall
+        )
+    }
+}
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun AffirmationsApp(modifier: Modifier = Modifier) {
+    AffirmationList(
+        DataSource().loadAffirmations(),
         modifier = modifier
     )
 }
 
+@SuppressLint("ResourceType")
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AffirmationsAppTheme {
-        Greeting("Android")
+        AffirmationsAppLayout()
     }
 }
